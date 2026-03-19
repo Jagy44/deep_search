1743d21a7d626cea369886129adba08283154bbb
Anzheyyy <jegeranze@gmail.com>
2026-03-19 16:29:42 +0000

    Izpis besedila dela.

--- out.cpp
+++ out.cpp
@@ -4,4 +4,7 @@ using namespace std;
 
 void izpisBesedila(vector<string> text){
 	//dodaj izpis array
+	for(int i = 0; i < text.size(); i++){
+		cout << "Beseda ki sledi je: " << text[i] << endl;
+	}
 }
